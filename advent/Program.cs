67b7d3180534da3f56116b76a04cf3f0b80ee702
notjo-hn <lubiszewskin19@ashfieldstudent.ttct.co.uk>
2024-12-18 10:31:53 +0000

    Day 2 Task 1 WORKING
    WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
    
    :D
    :D

--- advent/Program.cs
+++ advent/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.IO.IsolatedStorage;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
 string filename = "datafile.txt";
@@ -7,53 +9,82 @@ string filePath = Path.Combine("C:\\Users\\lubis\\Documents\\GitHub\\AdventOfCod
 string[] dataFromFile = File.ReadAllLines(filePath);
 //assigns each row to list
 List<string> list = [.. dataFromFile];
-int diff = 0;
 int SafeTotal = 0;
-bool safe = true;
-
-//true = inc, false = dec.
-bool incOrDec = true;
 
 
-foreach(string row in list)
+for (int i = 0; i < list.Count; i++)
 {
 
-    List<int> Numbers = row.Split(' ').Select(int.Parse).ToList();
-    for (int i = 0; i < Numbers.Count; i++)
-    {
-        if(i == 0)
-        {
-            diff = Numbers[i] - Numbers[i++];
-            if (diff > 0)
-            {
-                incOrDec = false;
-            }else if (diff < 0)
-            {
-                incOrDec = true;
-            }
-        }
-        
-        if(incOrDec && diff > 0)
-        {
-            safe = false;
-        } else if (incOrDec == false && diff < 0)
-        {
-            safe = false;
-        }
-        if (diff > 3 || diff < -3)
-        {
-            safe = false;
-        }
-    }
-    if (safe)
+    if (SingleDirectionChecker(i) && DifferenceChecker(i))
     {
         SafeTotal++;
     }
-    safe = true;
-    
 }
 
+
+
+
+bool SingleDirectionChecker(int x)  //make sure its all incrmementing or decremenitngnngrnhgrkh
+{
+    bool isSafe = true;
+    bool isIncrementing = true;
+
+    List<int> Numbers = list[x].Split(' ').Select(int.Parse).ToList();
+
+    if (Numbers[0] > Numbers[1])
+    {
+        isIncrementing = false;
+    }
+    else if (Numbers[0] < Numbers[1])
+    {
+        isIncrementing = true;
+    }
+
+    for (int i = 1; i < Numbers.Count - 1; i++)
+    {
+        //check here and once it Fails a check then it will change the IsSafe flag to false
+        if (isIncrementing)
+        {
+            if (Numbers[i] > Numbers[i + 1])
+            {
+                isSafe = false;
+
+            }
+        }
+        else
+        {
+            if (Numbers[i] < Numbers[i + 1])
+            {
+                isSafe = false;
+
+            }
+        }
+    }
+    return isSafe;
+}
+
+bool DifferenceChecker(int x)  //check difference to make sure difference is >= 1 & <4   1=<d<4
+{
+    bool isSafe = true;
+
+    List<int> Numbers = list[x].Split(' ').Select(int.Parse).ToList();
+    for (int i = 0; i < Numbers.Count - 1; i++)
+    {
+        if (Math.Abs((Numbers[i] - Numbers[i + 1])) > 3 || Math.Abs((Numbers[i] - Numbers[i + 1])) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 Console.WriteLine(SafeTotal);
+
+
+
+
+
 
 //foreach row in list
 //foreach number in Row
